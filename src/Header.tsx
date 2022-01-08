899608b502f45ebde1893abe6b4cd3c9f6aaea40
David Blackman <david@whizziwig.com>
2022-01-07 22:01:10 -0800

    initial take

---
+++ src/Header.tsx
@@ -0,0 +1,56 @@
+import React from "react";
+import { DateTime } from "luxon";
+
+import styled from "styled-components";
+
+const HeaderContainer = styled.div`
+  border-bottom: solid 3px ${(props) => props.theme.colors.borderColor};
+  border-right: solid 3px ${(props) => props.theme.colors.borderColor};
+  border-left: solid 3px ${(props) => props.theme.colors.borderColor};
+  box-sizing: border-box;
+  width: 100%;
+  margin-bottom: ;
+`;
+
+const HeaderComponents = styled.div`
+  display: flex;
+  flex-direction: row;
+`;
+
+const HeaderComponent = styled.div`
+  padding: 0.4em 0.4em 0.1em;
+  font-size: x-large;
+  font-weight: 400;
+  border-left: solid 1px ${(props) => props.theme.colors.borderColor};
+`;
+
+const HeaderComponentLeft = styled(HeaderComponent)`
+  border-left: unset;
+  flex-grow: 1;
+  justify-content: end;
+  text-align: end;
+`;
+
+export function Header() {
+  const date = DateTime.fromJSDate(new Date());
+  const quarter = date.toFormat("q");
+  const year = date.toFormat("yyyy");
+  const mon = date.toFormat("MMM");
+  const week = date.toFormat("W");
+  const daydate = date.toFormat("ccc, d");
+
+  const title = "Daily Plan";
+
+  return (
+    <HeaderContainer>
+      <HeaderComponents>
+        <HeaderComponent>{year}</HeaderComponent>
+        <HeaderComponent>Q{quarter}</HeaderComponent>
+        <HeaderComponent>{mon}</HeaderComponent>
+        <HeaderComponent>Week {week}</HeaderComponent>
+        <HeaderComponent>{daydate}</HeaderComponent>
+        <HeaderComponentLeft>{title}</HeaderComponentLeft>
+      </HeaderComponents>
+    </HeaderContainer>
+  );
+}
--- src/Page.tsx
+++ src/Page.tsx
@@ -1,27 +1,119 @@
 import React from "react";
-import tw from "tailwind-styled-components";
+import styled, { ThemeProvider } from "styled-components";
+import { Header } from "./Header";
 
-const BorderBox = tw.div`
-    border-2
-    rounded
-    w-full
-    h-full
+const theme = {
+  colors: {
+    backgroundColor: "#FFFDF9",
+    textColor: "#06B49A",
+    dotColor: "#06B49A",
+    borderColor: "#AFDBD2",
+  },
+};
+
+const BasicBorder = styled.div`
+  border: solid 1px ${(props) => props.theme.colors.borderColor};
+  border-radius: 10px;
+  background: ${(props) => props.theme.colors.backgroundColor};
+  overflow: hidden;
 `;
 
-const FullRow = tw.div`
-col-span-12
+const BorderBox = styled(BasicBorder)`
+  width: 100%;
+  height: 100%;
 `;
 
-function RuledBox() {
-  return <BorderBox />;
+const BoxTitle = styled(BasicBorder)`
+  position: absolute;
+  top: 0px;
+  left: 50%;
+  transform: translate(-50%, -1em);
+  padding: 0.2em;
+  white-space: nowrap;
+`;
+
+const PageGrid = styled.div`
+  display: grid;
+  grid-template-columns: repeat(12, [col-start] 1fr);
+  gap: 20px;
+  width: 100%;
+  height: 100%;
+  padding: 1.2em;
+`;
+
+const WithDots = styled.div`
+  background: linear-gradient(
+        90deg,
+        ${(props) => props.theme.colors.backgroundColor} 20px,
+        transparent 1%
+      )
+      center,
+    linear-gradient(
+        ${(props) => props.theme.colors.backgroundColor} 20px,
+        transparent 1%
+      )
+      center,
+    ${(props) => props.theme.colors.dotColor};
+  background-size: 22px 22px;
+
+  width: calc(100% - 20px);
+  height: calc(100% - 20px);
+  margin: 10px;
+`;
+
+const WithLines = styled.div`
+  background-image: linear-gradient(
+    0deg,
+    ${(props) => props.theme.colors.dotColor} 2.38%,
+    ${(props) => props.theme.colors.backgroundColor} 2.38%,
+    ${(props) => props.theme.colors.backgroundColor} 50%,
+    ${(props) => props.theme.colors.dotColor} 50%,
+    ${(props) => props.theme.colors.dotColor} 52.38%,
+    ${(props) => props.theme.colors.backgroundColor} 52.38%,
+    ${(props) => props.theme.colors.backgroundColor} 100%
+  );
+  background-size: 42px 42px;
+
+  width: 100%;
+  height: 100%;
+  // margin: 10px;
+`;
+
+function DottedBox({ className, title }: { className: string; title: string }) {
+  return (
+    <div className={`${className} relative`}>
+      <BoxTitle>{title}</BoxTitle>
+      <BorderBox>
+        <WithDots />
+      </BorderBox>
+    </div>
+  );
+}
+
+function RuledBox({ className, title }: { className: string; title: string }) {
+  return (
+    <div className={`${className} relative`}>
+      <BoxTitle>{title}</BoxTitle>
+      <BorderBox>
+        <WithLines />
+      </BorderBox>
+    </div>
+  );
 }
 
 export function Page() {
   return (
-    <div className="grid grid-cols-12 grid-rows-6 grid-flow-row-dense ">
-      <FullRow>
-        <RuledBox />
-      </FullRow>
-    </div>
+    <ThemeProvider theme={theme}>
+      <Header />
+      <PageGrid>
+        <RuledBox className="col-span-12" title="who is your daddy" />
+
+        <RuledBox className="col-span-4" title="who is your daddy" />
+        <RuledBox className="col-span-4" title="who is your daddy" />
+        <RuledBox className="col-span-4" title="who is your daddy" />
+
+        <DottedBox className="col-span-12" title="who is your daddy" />
+      </PageGrid>
+    </ThemeProvider>
   );
 }
