a33833a5813c8e9f50446b81e0e972d2e7e50c4b
David Blackman <david@whizziwig.com>
2022-01-17 20:48:13 -0800

    my own theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,9 +7,37 @@ module.exports = {
     themes: [
       {
         mytheme: {
-          primary: "#ea5234",
-          "primary-focus": "#d43616",
-          "primary-content": "#ffffff",
+          /* your theme name */ primary: "#a991f7" /* Primary color */,
+          "primary-focus": "#8462f4" /* Primary color - focused */,
+          "primary-content":
+            "#ffffff" /* Foreground content color to use on primary color */,
+
+          secondary: "#f6d860" /* Secondary color */,
+          "secondary-focus": "#f3cc30" /* Secondary color - focused */,
+          "secondary-content":
+            "#ffffff" /* Foreground content color to use on secondary color */,
+
+          accent: "#37cdbe" /* Accent color */,
+          "accent-focus": "#2aa79b" /* Accent color - focused */,
+          "accent-content":
+            "#ffffff" /* Foreground content color to use on accent color */,
+
+          neutral: "#3d4451" /* Neutral color */,
+          "neutral-focus": "#2a2e37" /* Neutral color - focused */,
+          "neutral-content":
+            "#ffffff" /* Foreground content color to use on neutral color */,
+
+          "base-100":
+            "#ffffff" /* Base color of page, used for blank backgrounds */,
+          "base-200": "#f9fafb" /* Base color, a little darker */,
+          "base-300": "#AFDBD2" /* Base color, even more darker */,
+          "base-content":
+            "#1f2937" /* Foreground content color to use on base color */,
+
+          info: "#2094f3" /* Info */,
+          success: "#009485" /* Success */,
+          warning: "#ff9900" /* Warning */,
+          error: "#ff5724" /* Error */,
         },
       },
     ],
