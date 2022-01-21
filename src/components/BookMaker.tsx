5882c2b0e1870724ce91945a133797fa64b174b6
David Blackman <david@whizziwig.com>
2022-01-20 23:15:29 -0800

    textadjustsafari

--- src/components/BookMaker.tsx
+++ src/components/BookMaker.tsx
@@ -61,7 +61,7 @@ function SpreadPreview({ pages }: { pages: GeneratedPages }) {
       zoomFactor = maxHeight / heightInPx;
     }
 
-    return { zoom: zoomFactor, WebkitTextSizeAdjust: "none" };
+    return { zoom: zoomFactor, WebkitTextSizeAdjust: `${zoomFactor * 100}%` };
   }, [printConfigValue, detectMobile, windowWidth]);
 
   const questionConfig = fullAppQuestionConfig.attach(Downgraded).get()[
