7d09572fcdbadc617958bfb6bfca3c2101a5171c
David Blackman <david@whizziwig.com>
2022-01-22 11:02:28 -0800

    fix printing

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import seedrandom from "seedrandom";
+
 import "./styles/index.css";
 import App from "./App";
+
+seedrandom(new Date().toString(), { global: true });
 
 ReactDOM.render(<App />, document.getElementById("content"));
--- src/types/untyped.d.ts
+++ src/types/untyped.d.ts
@@ -1,3 +1,4 @@
 declare module "react-hook-form-auto";
 declare module "rhfa-emergency-styles";
 declare module "use-mobile-detect-hook";
+declare module "seedrandom";
